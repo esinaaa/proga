8f65d48199874ae1556739bd6d8053d4fbca1f3d
tonya <esinaantonina.com>
2026-03-02 00:26:58 +0500

    08_garden

--- lab 1/08_garden.py
+++ lab 1/08_garden.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 # в саду сорвали цветы
 garden = ('ромашка', 'роза', 'одуванчик', 'ромашка', 'гладиолус', 'подсолнух', 'роза', )
 
 # на лугу сорвали цветы
 meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер', 'мак', 'одуванчик', 'ромашка', )
-
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 # создайте множество цветов, произрастающих в саду и на лугу
 # garden_set =
 # meadow_set =
@@ -23,3 +22,18 @@ meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер'
 
 # выведите на консоль те, которые растут на лугу, но не растут в саду
 # TODO здесь ваш код
+
+garden_set = set(garden)
+meadow_set = set(meadow)
+
+all = garden_set | meadow_set  
+print("Все виды цветов:", all)
+
+both = garden_set & meadow_set 
+print("Растут и в саду, и на лугу:", both)
+
+only_garden = garden_set - meadow_set
+print("Растут в саду, но не на лугу:", only_garden)
+
+only_meadow = meadow_set - garden_set
+print("Растут на лугу, но не в саду:", only_meadow)
