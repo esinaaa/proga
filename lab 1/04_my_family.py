b6a0aa30da6a9a80913cb5ad0fa8b85f54b3d0da
tonya <esinaantonina.com>
2026-03-01 22:59:37 +0500

    04_my_family

--- lab 1/04_my_family.py
+++ lab 1/04_my_family.py
@@ -1,20 +1,23 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 # Создайте списки:
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
-my_family = []
+my_family = ["Мама", "Папа", "Я"]
 
-
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 # список списков приблизителного роста членов вашей семьи
 my_family_height = [
-    # ['имя', рост],
-    [],
+    ["Мама", 164],
+    ["Папа", 187],
+    ["Я", 162]
 ]
 
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
-
+Post_papa = my_family_height[1][1]
+print("Рост отца", Post_papa, "см")
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+Post= my_family_height[0][1]+my_family_height[1][1]+my_family_height[2][1]
+print("Рост семьи", Post, "см")
